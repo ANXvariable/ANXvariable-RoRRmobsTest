91d1af227bac529f524f5eccea6ddf07ed8a7f68
(V)ariable <77659883+ANXvariable@users.noreply.github.com>
2025-06-05 13:56:30 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,32 @@
+--monsters
+
+1og. info ("Loading ".._ENV["!guid"]..".")
+local envy = mods ["LuaENVY-ENVY" ]
+envy.auto()
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto(true)
+
+PATH = _ENV["!plugins_mod_folder_path"]
+NAMESPACE = "anx"
+
+local initialize = function()
+	local folders = {
+		"Actors",
+		"Elites"
+	}
+
+	for _, folder in ipairs(folders) do
+		local filepaths = path.get_files(path.combine(PATH, folder))
+		for _, filepath in ipairs(filepaths) do
+			-- filter for files with the .lua extension
+			if string.sub(filepath, -4, -1) == ".lua" then
+				require(filepath)
+			end
+		end
+	end
+
+end
+Initialize(initialize)
+
+-- ** Uncomment the two lines below to re-call initialize on hotload
+if hotload then initialize() end
+hotload = true
